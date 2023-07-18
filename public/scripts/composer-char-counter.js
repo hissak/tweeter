83f2754795e0ff9464f83e6080c49458653bb42e
vagrant <hassan.a.issak@gmail.com>
2023-07-18 21:07:33 +0000

    Fix starting value of character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,18 +2,18 @@
 let charCount = 0
 
 $(document).ready(function() {
-  let charLimit = 10;
+  let charLimit = 150;
   let availableChars = 0;
+  $('#counter').text(`${charLimit} characters left`)
 
   $('#tweet-text').on('input', function(event) {
     availableChars = charLimit - $(this).val().length;
     console.log(availableChars);
-    const counter = $(this).parent().parent().find(".counter");
-
+    const counter = $('#counter');
     if(availableChars < 0) {
-      counter.text(availableChars + ' characters left').css('color', 'red')
+      $(counter).text(availableChars + ' characters left').css('color', 'red')
   } else {
-    counter.text(availableChars + ' characters left').css('color', 'black')
+    $(counter).text(availableChars + ' characters left').css('color', 'black')
   }
   })
 }
