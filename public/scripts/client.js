57400c63fa39e724ffa0220afc6641cb3289eb93
vagrant <hassan.a.issak@gmail.com>
2023-07-19 18:54:09 +0000

    Define loadTweets function that loads all tweets from /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -32,54 +32,62 @@ const createTweetElement = function(tweet) {
 }
 
 const renderTweets = function(tweets) {
-  let tweetArray = tweets.map(createTweetElement);
-  return tweetArray.map(function(tweet){
-    return $('.tweet-container').append(tweet)
-  })
+  const tweetContainer = $('.tweet-container').html('')
+  let tweetArray = tweets.map((tweet) => {
+    const tweetElement = createTweetElement(tweet);
+    return tweetContainer.append(tweetElement)
+  });
 }
 
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+// const data = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png"
+//         ,
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd" },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     }
+//   ]
 // const $tweet = createTweetElement(tweetData);
 
 // Test / driver code (temporary)
 // console.log($tweet); // to see what it looks like
 // $('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-renderTweets(data);
+// renderTweets(data);
+
+const loadTweets = function(api) {
+  $.get(api).then(function(res){
+    console.log(res);
+    renderTweets(res);
+  })
+}
+loadTweets('/tweets');
 
 $('.Submit-tweet').on("submit", function(event){ // must always target the form element and not the button directly
   event.preventDefault();
   console.log('Prevent default behaviour!');
   const url = $(this).attr('action');
-  const data = $(this).serialize(); 
-  $.post(url, data).done(function(result) {//ASK MENTOR ABOUT {DATA:DATA}
+  const data = $(this).serialize(); //Instead of this, maybe event.currentTarget?
+  console.log('This! ==> ', data)
+  $.post(url, data).then(function(result) {
     console.log('Tweet data below');
-    console.log(data);
-    console.log(result);
+    console.log('Result ==> ', result);
   })
 })
-
 })
 
