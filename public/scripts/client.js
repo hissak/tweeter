02a966ca4592bbdd49118bac70b3ac0346ab7cd5
vagrant <hassan.a.issak@gmail.com>
2023-07-19 20:15:23 +0000

    Add post functionality where user  tweet appears at top of tweet list without having to refresh

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,8 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-// const $tweet = $(`<article class="tweet">Hello world</article>`);
 $(document).ready(function() {
 
 const createTweetElement = function(tweet) {
@@ -35,40 +33,11 @@ const renderTweets = function(tweets) {
   const tweetContainer = $('.tweet-container').html('')
   let tweetArray = tweets.map((tweet) => {
     const tweetElement = createTweetElement(tweet);
-    return tweetContainer.append(tweetElement)
+    return tweetContainer.prepend(tweetElement)
   });
 }
 
-// const data = [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": "https://i.imgur.com/73hZDYK.png"
-//         ,
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": "https://i.imgur.com/nlhLi3I.png",
-//         "handle": "@rd" },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1461113959088
-//     }
-//   ]
-// const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-// renderTweets(data);
 
 const loadTweets = function(api) {
   $.get(api).then(function(res){
@@ -108,6 +77,7 @@ $('.Submit-tweet').on("submit", function(event){ // must always target the form 
   $.post(url, data).then(function(result) {
     console.log('Tweet data below');
     console.log('Result ==> ', result);
+    loadTweets('/tweets');
     $('.Submit-tweet')[0].reset();
   })
 }
