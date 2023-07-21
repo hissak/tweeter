158586d0eae13c229a65528bbee2f39b74d78abf
vagrant <hassan.a.issak@gmail.com>
2023-07-21 02:44:59 +0000

    Add comments to all stylesheets and JS files. Lint all code.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,16 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
+  // Creates the html for a tweet by pulling data from specified database.
+  // Sanitizes input to avoid XSS.
+  const createTweetElement = function(tweet) {
+    const escape = function(str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
 
-const createTweetElement = function(tweet) {
-
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-  
-  const $tweet = `<article class="tweet">
+    const $tweet = `<article class="tweet">
           <header>
             <p id="tweet-header-p1">
               <img src=${escape(tweet.user.avatars)}>
@@ -33,51 +33,56 @@ const createTweetElement = function(tweet) {
             </p>
           </footer>
         </article>`;
-        return $tweet;
-}
+    return $tweet;
+  };
 
+// Renders all the tweets in a database for display.
+  const renderTweets = function(tweets) {
+    const tweetContainer = $('.tweet-container').html('');
+    tweets.map((tweet) => {
+      const tweetElement = createTweetElement(tweet);
+      return tweetContainer.append(tweetElement);
+    });
+  };
 
-const renderTweets = function(tweets) {
-  const tweetContainer = $('.tweet-container').html('')
-  let tweetArray = tweets.map((tweet) => {
-    const tweetElement = createTweetElement(tweet);
-    return tweetContainer.append(tweetElement)
+// Performs a GET request for tweet data, then uses createTweetElement and renderTweets
+// to display the tweets in reverse order.
+
+  const loadTweets = function(api) {
+    $.get(api).then(function(res) {
+      const reversedRes = res.reverse();
+      console.log(reversedRes);
+      renderTweets(reversedRes);
+    });
+  };
+
+  loadTweets('/tweets');
+
+// Carries out a POST request when a tweet is submitted. Fetches the updated
+// list of tweets and displays it. Resets the form and character counter.
+  $('.Submit-tweet').on("submit", function(event) {
+    event.preventDefault();
+    console.log('Prevent default behaviour!');
+    const url = $(this).attr('action');
+    const data = $(this).serialize();
+    const form = $('#tweet-text').val();
+    console.log('This! ==> ', form);
+    if (!form) {
+      $('.error-message').html('<i class="fa-solid fa-circle-exclamation"></i> Cannot post a blank tweet!').fadeIn('slow');
+      return false;
+    } else if (form.length > 140) {
+      return false;
+    } else {
+      $('.error-message').fadeOut('slow');
+    }
+
+    $.post(url, data).then(function(result) {
+      console.log('Tweet data below');
+      console.log('Result ==> ', result);
+      loadTweets('/tweets');
+      $('.Submit-tweet')[0].reset();
+      $('#counter').text(`140 characters left`);
+    });
   });
-}
-
-
-
-const loadTweets = function(api) {
-  $.get(api).then(function(res){
-    const reversedRes = res.reverse();
-    console.log(reversedRes);
-    renderTweets(reversedRes);
-  })
-}
-
-loadTweets('/tweets');
-
-$('.Submit-tweet').on("submit", function(event){ 
-  event.preventDefault();
-  console.log('Prevent default behaviour!');
-  const url = $(this).attr('action');
-  const data = $(this).serialize(); 
-  const form = $('#tweet-text').val();
-  console.log('This! ==> ', form)
-  if(!form) {
-    $('.error-message').html('<i class="fa-solid fa-circle-exclamation"></i> Cannot post a blank tweet!').fadeIn('slow');
-    return false
-  } else if (form.length > 140){
-  return false
-} else { $('.error-message').fadeOut('slow'); };
-
-  $.post(url, data).then(function(result) {
-    console.log('Tweet data below');
-    console.log('Result ==> ', result);
-    loadTweets('/tweets');
-    $('.Submit-tweet')[0].reset();
-    $('#counter').text(`140 characters left`);
-  })
-})
-})
+});
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,21 +3,24 @@
 $(document).ready(function() {
   let charLimit = 140;
   let availableChars = 0;
+
   $('#counter').text(`${charLimit} characters left`);
 
+  //Updates the number of characters left in real-time. Displays an error message if
+  // character cout is exceeded. Updates in real-time if charcater count is less than 140.
   $('#tweet-text').on('input', function(event) {
     availableChars = charLimit - $(this).val().length;
     console.log(availableChars);
     const counter = $('#counter');
-    if(availableChars < 0) {
+    if (availableChars < 0) {
       $(counter).text(availableChars + ' characters left').css('color', 'red');
-      $('.error-message').html('<i class="fa-solid fa-circle-exclamation"></i> Character limit exceeded!').fadeIn('slow'); 
+      $('.error-message').html('<i class="fa-solid fa-circle-exclamation"></i> Character limit exceeded!').fadeIn('slow');
       $('.error-message').css('display', 'block');
     } else {
       $('.error-message').fadeOut('slow');
-      $(counter).text(availableChars + ' characters left').css('color', '#545149')
+      $(counter).text(availableChars + ' characters left').css('color', '#545149');
     }
-  })
+  });
 }
 );
 
